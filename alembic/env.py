5eed4c9c89da2ef6a22b2c98807c97dc5688a55a
Nhan Nguyen <36336994+NhanNguyen001@users.noreply.github.com>
2024-12-04 11:38:45 +0700

    update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -6,10 +6,14 @@ from sqlalchemy import pool
 from alembic import context
 
 import models
+from config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# Set the sqlalchemy url from environment variable
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
