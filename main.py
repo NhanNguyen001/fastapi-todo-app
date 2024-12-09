6e35a6787cfb7230551aabb63133e6b7db325499
Nhan Nguyen <nhan2151@gmail.com>
2024-12-09 11:23:15 +0700

    Update main.py
    Add global exception_handle

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, HTTPException
 from models import Base
 from database import engine
 from routers import auth, todos, admin, users
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, JSONResponse
 from monitoring import setup_monitoring
 import logging
 
@@ -38,3 +38,10 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
 app.include_router(users.router)
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail}
+    )
